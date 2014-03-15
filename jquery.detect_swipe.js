e7c678f3b24a653dfe344dd2effe0bb409a900c7
Marc-Andre Lafortune <github@marc-andre.ca>
2014-03-14 22:42:13 -0400

    Rename & update header

--- jquery.detect_swipe.js
+++ jquery.detect_swipe.js
@@ -1,14 +1,11 @@
 /**
- * jQuery Plugin to obtain touch gestures from iPhone, iPod Touch and iPad, should also work with Android mobile phones (not tested yet!)
- * Common usage: wipe images (left and right to show the previous or next image)
- *
- * @author Andreas Waltl, netCU Internetagentur (http://www.netcu.de)
- * @version 1.1.1 (9th December 2010) - fix bug (older IE's had problems)
- * @version 1.1 (1st September 2010) - support wipe up and wipe down
- * @version 1.0 (15th July 2010)
+ * jquery.detectSwipe v1.0
+ * jQuery Plugin to obtain touch gestures from iPhone, iPod Touch, iPad and Android
+ * http://github.com/marcandre/detect_swipe
+ * Based on touchwipe by Andreas Waltl, netCU Internetagentur (http://www.netcu.de)
  */
 (function($) {
-  $.fn.touchwipe = function(settings) {
+  $.fn.detectSwipe = function(settings) {
     var config = {
       threshold: 20,
       preventDefaultEvents: true
